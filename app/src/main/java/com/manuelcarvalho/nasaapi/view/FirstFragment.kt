80a92405fc3a027655cbf363764de55e41e0e6f6
AndroidOS <burrowsgit@gmail.com>
2021-01-20 07:58:14 +1100

    Added viewmodel observer

--- app/src/main/java/com/manuelcarvalho/nasaapi/view/FirstFragment.kt
+++ app/src/main/java/com/manuelcarvalho/nasaapi/view/FirstFragment.kt
@@ -1,16 +1,20 @@
 package com.manuelcarvalho.nasaapi.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.manuelcarvalho.nasaapi.R
+import com.manuelcarvalho.nasaapi.viewmodel.NasaViewModel
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+private const val TAG = "FirstFragment"
 class FirstFragment : Fragment() {
+
+    private lateinit var viewModel: NasaViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +23,29 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = activity?.run {
+            ViewModelProviders.of(this)[NasaViewModel::class.java]
+        } ?: throw Exception("Invalid Activity")
+
+        observeViewModel()
+
+    }
+
+    private fun observeViewModel() {
+
+
+        viewModel.photoList.observe(viewLifecycleOwner, Observer { listURL ->
+            listURL?.let {
+                Log.d(TAG, "${it}")
+            }
+        })
+
+
+    }
+
 
 }
